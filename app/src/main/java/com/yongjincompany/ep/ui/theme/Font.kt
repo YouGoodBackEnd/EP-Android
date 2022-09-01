7672f8010a3fa976e74697efbf6612d7230c3397
Leesin0222 <dragon02229@naver.com>
2022-09-02 08:51:17 +0900

    ⚡ :: Font사용하기 위한 NotoSans val(상수) 생성

---
+++ app/src/main/java/com/yongjincompany/ep/ui/theme/Font.kt
@@ -0,0 +1,12 @@
+package com.yongjincompany.ep.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.yongjincompany.ep.R
+
+val NotoSansKR = FontFamily(
+    Font(R.font.notosanskrmedium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.notosanskrbold, FontWeight.Bold, FontStyle.Normal)
+)
